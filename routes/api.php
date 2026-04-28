fc5a0aaf3fc779c2a928445e02a5c63f4e3fe8c7
Usman Aslam <usmanaslam805@gmail.com>
2026-04-28 14:16:29 +0500

    Exercise 02

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Exercise\Exercise01Controller;
+use App\Http\Controllers\Exercise\Exercise02Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -7,3 +9,5 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::post('exercise-1-artwork-version', [Exercise01Controller::class, 'post']);
+Route::post('exercise-2-tier-pricing', [Exercise02Controller::class, 'post']);
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,3 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Exercise\Exercise01Controller;
-use App\Http\Controllers\Exercise\Exercise02Controller;
-
-Route::withoutMiddleware('web')->group(function () {
-    Route::post('exercise-1-artwork-version', [Exercise01Controller::class, 'post']);
-    Route::post('exercise-2-tier-pricing', [Exercise02Controller::class, 'post']);
-});
