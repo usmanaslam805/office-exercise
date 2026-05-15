ab8e830a57036fccd765af46ead0d754a2b13078
Usman Aslam <usmanaslam805@gmail.com>
2026-05-15 19:44:57 +0500

    Exercise 14

---
+++ app/Http/Controllers/Exercise/Exercise14Controller.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Exercise;
+
+use App\Http\Controllers\Controller;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class Exercise14Controller extends Controller
+{
+    public function post(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'input' => 'required|array',
+
+            'input.nums' => 'present|array',
+            'input.nums.*' => 'integer|numeric:strict|min:1',
+
+            'input.target' => 'integer|numeric:strict|min:0',
+        ]);
+
+        $response = [
+            'success' => false,
+            'error' => null
+        ];
+
+        if ($validator->fails()) {
+            $response['message'] = "Validation error";
+            $response['error'] = $validator->messages()->first();
+
+            return response()->json($response);
+        }
+
+        $input = $request['input'];
+        $target = $input['target'];
+        $numbersArray = $input['nums'];
+        $outputArray = [];
+
+        foreach ($numbersArray as $key => $value) {
+            if ($value + $numbersArray[$key + 1] == $target) {
+                $outputArray = [$key, $key + 1];
+                break;
+            }
+        }
+
+        if (!isset($outputArray[0])) {
+            foreach ($numbersArray as $key01 => $value01) {
+                foreach ($numbersArray as $key02 => $value02) {
+                    if (($value01 + $value02) == $target) {
+                        $outputArray = [$key01, $key02];
+                        break;
+                    }
+                }
+                if (isset($outputArray[0])) {
+                    break;
+                }
+            }
+        }
+
+        $response['success'] = true;
+        $response['data'] = $outputArray;
+
+        return response()->json($response);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,7 @@ use App\Http\Controllers\Exercise\Exercise09Controller;
 use App\Http\Controllers\Exercise\Exercise10Controller;
 use App\Http\Controllers\Exercise\Exercise11Controller;
 use App\Http\Controllers\Exercise\Exercise13Controller;
+use App\Http\Controllers\Exercise\Exercise14Controller;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -32,3 +33,6 @@ Route::post('exercise-9-webhook', [Exercise09Controller::class, 'post']);
 Route::post('exercise-10-quote-expiry', [Exercise10Controller::class, 'post']);
 Route::post('exercise-11-product-visibility', [Exercise11Controller::class, 'post']);
 Route::post('exercise-13-cart-merge', [Exercise13Controller::class, 'post']);
+Route::post('exercise-14-upsell', [Exercise14Controller::class, 'post']);
+
+
